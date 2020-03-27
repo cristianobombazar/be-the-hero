0e7a0f1e1acf63ca239d0ab49c39177d7197db26
Cristiano Bombazar <cristiano.bombazar@integritas.net>
2020-03-27 09:54:19 -0300

    Validation using celebrate

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate')
 const ongController = require('./controllers/NgoController');
 const incidentController = require('./controllers/IncidentController');
 const profileController = require('./controllers/ProfileController');
@@ -7,11 +8,37 @@ const sessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.post('/session', sessionController.create);
+
 routes.get('/ngo', ongController.findAll);
-routes.post('/ngo', ongController.create);
+routes.post('/ngo', celebrate({
+
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+
+}), ongController.create);
+
 routes.post('/incident', incidentController.create);
-routes.get('/incident', incidentController.findAll);
-routes.delete('/incident/:id', incidentController.deleteById);
-routes.get('/profile', profileController.findAllByNgo);
+routes.get('/incident', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}),incidentController.findAll);
+
+routes.delete('/incident/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), incidentController.deleteById);
+
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}), profileController.findAllByNgo);
 
 module.exports = routes;
